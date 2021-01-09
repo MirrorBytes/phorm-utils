e729de6ef403b28275a498ccba6874b056b6b99d
MirrorBytes <smokebytes@gmail.com>
2021-01-08 22:23:03 -0500

    chore(docs): add docs for ToC and expose it

--- packages/multi/runtime/index.ts
+++ packages/multi/runtime/index.ts
@@ -3,3 +3,4 @@ export * from './types';
 export { default as Form } from './wrappers/Form.svelte';
 export { default as Field } from './wrappers/Field.svelte';
 export { default as Step } from './wrappers/Step.svelte';
+export { default as ToC } from './organization/ToC.svelte';
